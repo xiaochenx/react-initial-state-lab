048c17557a7415067fdd5b67187be8b9b60d9f62
Xiaochen Xu <xiaochex@gmail.com>
2021-04-24 12:26:54 -0400

    commit tree 6fc46f94436e4b25d1f330195e2bfed292e4aed2

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    bombMessage = () => {
+        if (this.state.secondsLeft === 0){
+            return <h3>Boom!</h3>
+        }else{
+        return <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+             
+                  {this.bombMessage()}
+                
+            </div>
+        )
+    }
+}
